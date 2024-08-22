f03ab08dac8fdd396c007795333b4086d0e7e318
Michał Boryczko <michal@MacBook-Pro.local>
2024-08-22 16:51:53 +0200

    refs #MAS-1761 - some cr fixes

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 final class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         return new TreeBuilder('mms_idempotent_consumer_symfony_messenger');
     }
